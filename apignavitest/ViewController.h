701041bdc5eb809c0544e9b080ea46e5e00742af
DotaKobayashi <dota.kobayashi@gmail.com>
2014-05-29 23:16:56 +0900

    Initial Commit

---
+++ apignavitest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  apignavitest
+//
+//  Created by 小林堂太 on 2014/05/29.
+//  Copyright (c) 2014年 dota.kobayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ apignavitest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  apignavitest
+//
+//  Created by 小林堂太 on 2014/05/29.
+//  Copyright (c) 2014年 dota.kobayashi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

85c8a40d306c5ebaaecbeb985161b61a08ba3bac
DotaKobayashi <dota.kobayashi@gmail.com>
2014-05-29 23:22:44 +0900

    NSURLSession

--- apignavitest/ViewController.m
+++ apignavitest/ViewController.m
@@ -12,6 +12,8 @@
 
 @end
 
+NSString *const AccessKey = @"hoge";
+
 @implementation ViewController
 
 - (void)viewDidLoad
@@ -24,6 +26,33 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
+                                                          delegate:self
+                                                     delegateQueue:[NSOperationQueue mainQueue]];
+    
+    NSString *api = [NSString stringWithFormat:@"http://api.gnavi.co.jp/ver1/RestSearchAPI/?keyid=%@&latitude=35.681382&longitude=139.766084&range=3", AccessKey];
+    
+    NSURL *url = [NSURL URLWithString:api];
+    
+    NSURLSessionTask *task = [session dataTaskWithURL:url];
+    
+    [task resume];
+
+}
+
+#pragma mark -
+#pragma mark NSURLSessionDataDelegate
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
+{
+    // NSData → NSString 変換
+    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", str);
 }
 
 @end
